cc019fb9878b705a71d54a093ce1ef2c604caae9
DylanDVDs <dvd00001@mix.wvu.edu>
2025-05-19 11:12:44 -0400

    Changed the variable names

--- js/script.js
+++ js/script.js
@@ -11,25 +11,25 @@ fetch("navbar.html")
 
 /*Dark/light button implementation*/
 function LnDModeSetup() {
-    let darkmode = localStorage.getItem("darkmode")
+    let darkMode = localStorage.getItem("darkMode")
     const darkLight = document.getElementById("dark-light")
 
     const enableDarkMode = () => {
-        document.body.classList.add("darkmode")
-        localStorage.setItem("darkmode", 'active')
+        document.body.classList.add("darkMode")
+        localStorage.setItem("darkMode", 'active')
     }
 
     const disableDarkMode = () => {
-        document.body.classList.remove("darkmode")
-        localStorage.setItem("darkmode", null)
+        document.body.classList.remove("darkMode")
+        localStorage.setItem("darkMode", null)
     }
 
-    if (darkmode === "active") {
+    if (darkMode === "active") {
         enableDarkMode()
     }
 
     darkLight.addEventListener("click", () => {
-        darkmode = localStorage.getItem("darkmode")
-        darkmode !== "active" ? enableDarkMode() : disableDarkMode()
+        darkMode = localStorage.getItem("darkMode")
+        darkMode !== "active" ? enableDarkMode() : disableDarkMode()
     })
 }
