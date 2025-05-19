e4994c7f1c4a8242096705bd598a0d848e61a275
DylanDVDs <dvd00001@mix.wvu.edu>
2025-05-19 08:28:13 -0400

    Turned navbar into its own page to reduce redundancy.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,35 @@
 
-let darkmode = localStorage.getItem("darkmode")
-const darkLight = document.getElementById("dark-light")
+/*navbar*/
+fetch("navbar.html")
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("navbar-container").innerHTML = data;
 
-const enableDarkMode = () => {
-    document.body.classList.add("darkmode")
-    localStorage.setItem("darkmode", 'active')
+        LnDModeSetup();
+    });
+
+
+/*Dark/light button implementation*/
+function LnDModeSetup() {
+    let darkmode = localStorage.getItem("darkmode")
+    const darkLight = document.getElementById("dark-light")
+
+    const enableDarkMode = () => {
+        document.body.classList.add("darkmode")
+        localStorage.setItem("darkmode", 'active')
+    }
+
+    const disableDarkMode = () => {
+        document.body.classList.remove("darkmode")
+        localStorage.setItem("darkmode", null)
+    }
+
+    if (darkmode === "active") {
+        enableDarkMode()
+    }
+
+    darkLight.addEventListener("click", () => {
+        darkmode = localStorage.getItem("darkmode")
+        darkmode !== "active" ? enableDarkMode() : disableDarkMode()
+    })
 }
-
-const disableDarkMode = () => {
-    document.body.classList.remove("darkmode")
-    localStorage.setItem("darkmode", null)
-}
-
-if (darkmode === "active") {
-    enableDarkMode()
-}
-
-darkLight.addEventListener("click", () => {
-    darkmode = localStorage.getItem("darkmode")
-    darkmode !== "active"? enableDarkMode() : disableDarkMode()
-})
